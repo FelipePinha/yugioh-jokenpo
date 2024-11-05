8ab58e40ba4891782ef9bfb030f604a42374e153
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-11-05 18:53:10 -0300

    add bgm

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -190,8 +190,12 @@ async function playAudio(status) {
 
 function init() {
     showHiddenCardFieldsImages()
+
     drawCards(5, state.playerSides.player1)
     drawCards(5, state.playerSides.computer)
+
+    const bgm = document.getElementById('bgm')
+    bgm.play()
 }
 
 init()
